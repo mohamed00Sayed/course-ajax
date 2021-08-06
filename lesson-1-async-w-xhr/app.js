c994ffd7993c3666129c7be5e0256187fd1fd01d
mohamed00Sayed <87094481+mohamed00Sayed@users.noreply.github.com>
2021-08-06 18:07:24 +0200

    Update app.js

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -9,4 +9,17 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
     });
+	
+	searchedForText = 'hippos';
+	const unsplashRequest = new XMLHttpRequest();
+
+	unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+	unsplashRequest.onload = addImage;
+	unsplashRequest.setRequestHeader('Authorization', 'Client-ID lQOJO5Jpn5zPIwURtogMiMp9dQwd8GF5fvgz6Dbpc-Y');
+	unsplashRequest.send();
+
+	function addImage(){
+		
+	}
+	
 })();
