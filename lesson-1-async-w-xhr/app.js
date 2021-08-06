3c4ee750973ed9488703b396543705605d1f4e2c
mohamed00Sayed <87094481+mohamed00Sayed@users.noreply.github.com>
2021-08-06 19:40:32 +0200

    Update app.js

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -3,20 +3,25 @@
     const searchField = document.querySelector('#search-keyword');
     let searchedForText;
     const responseContainer = document.querySelector('#response-container');
+	const searchButton = document.querySelector('#submit-btn');
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+		//first request to Unsplash
+		const unsplashRequest = new XMLHttpRequest();
+		unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+		unsplashRequest.onload = addImage;
+		unsplashRequest.setRequestHeader('Authorization', 'Client-ID lQOJO5Jpn5zPIwURtogMiMp9dQwd8GF5fvgz6Dbpc-Y');
+		unsplashRequest.send();
+		
+		//second request to NYTimes
+		const articleRequest = new XMLHttpRequest();
+		articleRequest.onload = addArticles;
+		articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=FyhFXnLTxSJtRiOAvFlGjH7g67ahUSad`);
+		articleRequest.send();
     });
-	
-	searchedForText = 'hippos';
-	const unsplashRequest = new XMLHttpRequest();
-
-	unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-	unsplashRequest.onload = addImage;
-	unsplashRequest.setRequestHeader('Authorization', 'Client-ID lQOJO5Jpn5zPIwURtogMiMp9dQwd8GF5fvgz6Dbpc-Y');
-	unsplashRequest.send();
 	
 	function addImage(){
 		const data = JSON.parse(this.responseText);
@@ -30,9 +35,17 @@
 	
 	function addArticles () {
 		let htmlContent = '';
+		const data = JSON.parse(this.responseText);
+		if(data.response && data.response.docs && data.response.docs.length > 1){
+			htmlContent = '<ul>'+ data.response.docs.map(
+				article => `<li class="article">
+					<h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+					<p>${article.snippet}</p>
+				</li>`
+			).join('')+'</ul>';			
+		}else{
+			htmlContent = '<div class="error-no-articles">No articles available</div>';
+		}
+		responseContainer.insertAdjacentHTML('beforeend', htmlContent);
 	}
-	const articleRequest = new XMLHttpRequest();
-	articleRequest.onload = addArticles;
-	articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=FyhFXnLTxSJtRiOAvFlGjH7g67ahUSad`);
-	articleRequest.send();
 })();
