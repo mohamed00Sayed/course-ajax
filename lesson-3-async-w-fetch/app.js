bc0b3bf76b69f23bccf2570975371a90b7796b76
mohamed00Sayed <87094481+mohamed00Sayed@users.noreply.github.com>
2021-08-07 18:59:47 +0200

    Update app.js

--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -3,10 +3,45 @@
     const searchField = document.querySelector('#search-keyword');
     let searchedForText;
     const responseContainer = document.querySelector('#response-container');
+	const searchButton = document.querySelector('#submit-btn');
 
-    form.addEventListener('submit', function (e) {
+    form.addEventListener('submit', async function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+		
+		fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+			headers: {
+				Authorization: 'Client-ID lQOJO5Jpn5zPIwURtogMiMp9dQwd8GF5fvgz6Dbpc-Y'
+			}
+		}).then(function(response) {
+			return response.json();
+		}).then(addImage);
     });
+	
+	function addImage(data){
+		const firstImage = data.results[0];
+		
+		let htmlContent = `<figure>
+			<img src="${firstImage.urls.regular}" alt="${searchedForText}">
+			<figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+		</figure>`;
+		responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+	}
+
+	function addArticles (data) {
+		let htmlContent = '';
+		if(data.response && data.response.docs && data.response.docs.length > 1){
+			htmlContent = '<ul>'+ data.response.docs.map(
+				article => `<li class="article">
+					<h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+					<p>${article.snippet}</p>
+				</li>`
+			).join('')+'</ul>';			
+		}else{
+			htmlContent = '<div class="error-no-articles">No articles available</div>';
+		}
+		responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+	}
+	
 })();
